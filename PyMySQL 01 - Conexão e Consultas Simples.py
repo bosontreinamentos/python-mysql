0da753b445d5162d5cf84760c0ab2ac9052e4932
Fábio dos Reis <fabiodosreis@hotmail.com>
2021-04-07 13:37:14 -0300

    Add files via upload

--- PyMySQL 01 - Conexão e Consultas Simples.py
+++ PyMySQL 01 - Conexão e Consultas Simples.py
@@ -1,23 +1,28 @@
 import pymysql.cursors
 
-con = pymysql.connect(host='localhost',user='root',password='abc123**',database='db_MeusLivros',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+# Abrir uma conexão a um banco de dados
+con = pymysql.connect(host='localhost',user='root',database='db_MeusLivros',cursorclass=pymysql.cursors.DictCursor,password='abc123**')
 
+# Preparar um cursor com o método .cursor()
 with con.cursor() as c:
-    # Ler um registro único
+    # Criar ma consulta e executá-la no banco
     sql = "SELECT NomeLivro, ISBN13 FROM tbl_livros WHERE IdLivro = 104"
     c.execute(sql)
     res = c.fetchone()
     print(res)
+    # Acessar dado pelo nome da coluna
+    print('Livro retornado:', res['NomeLivro'])
 
-
+print()
 with con.cursor() as c:
     # Retornar todas as linhas da tabela de editoras
     sql = "SELECT NomeEditora FROM tbl_editoras"
     c.execute(sql)
     res = c.fetchall()
     print(res)
-
+    print()
     for linha in res:
         print(linha['NomeEditora'])
 
-con.close
+# Desconectar do servidor
+con.close()
